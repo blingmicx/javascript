7116ed58bfcb3c3920ab49acbe8f752524020481
daltolomicaela <micaela.daltolo@davinci.edu.ar>
2023-04-07 15:35:29 -0300

    arreglos

--- new.js
+++ new.js
@@ -9,8 +9,8 @@ function createCards(data) {
         const card = document.createElement("div");
         card.classList.add("card");
 
-        const titulo = document.createElement("h2");
-        titulo.textContent = item.titulo;
+        const sucursal = document.createElement("h2");
+        sucursal.textContent = item.sucursal;
 
         const descripcion = document.createElement("p");
         descripcion.textContent = item.descripcion;
@@ -18,54 +18,66 @@ function createCards(data) {
         const imagen = document.createElement("img");
         imagen.src = item.imagen;
 
+        const boton = document.createElement("button");
+        boton.textContent = "Agendar turno!";
 
-        card.appendChild(titulo);
+        card.appendChild(sucursal);
         card.appendChild(descripcion);
         card.appendChild(imagen);
+        card.appendChild(boton);
 
         container.appendChild(card);
+
+        card.classList.add("card-style");
+
     });
 }
 
 //informacion de las card
 const data = [
     {
-        titulo: "Caballito",
+        sucursal: "Caballito",
         descripcion: "Local ubicado en sobre la conocida avenida Rivadavia y Miro, cuenta con mas de 15 gatitos para disfrutar tu estadia.",
         imagen: "./img/caballito.jpeg"
     },
     {
-        titulo: "Palermo",
+        sucursal: "Palermo",
         descripcion: "Nuestro primer local ubicado en la calle Honduras es el que mas felinos tiene gracias al refugio en colaboracion que tenemos con nuestros colaboradores de @MascotasPalermom",
         imagen: "./img/palermo.jpeg"
     },
     {
-        titulo: "Belgrano",
+        sucursal: "Belgrano",
         descripcion: "Nuestra gran apertura cerca de la estacion Echeverria y que en su mayoria esta integrada por gatitos en pleno crecimiento.",
         imagen: "./img/belgrano.jpeg"
     }
 ];
 
-//ocultar las card antes del inicio de sesion
 
+// función para mostrar las cards si el usuario inicia sesión
 
+//datos que tienen que coicidir
+function showCards() {
+    const passGuardado = "123456";
+    const emailGuardado = "usuario@mail.com";
+    const emailUsuario = document.getElementById("emailAddress");
+    const passUsuario = document.getElementById("password");
 
-// inico de la sesion con datos guardados
-
-let emailUsuario = document.getElementById('emailAddress');
-let passUsuario = document.getElementById('password');
-let btnLogin = document.getElementById('btnLogin');
-btnLogin.addEventListener('click', () => {
-    localStorage.setItem('email', emailUsuario.value);
-    localStorage.setItem('pass', passUsuario.value);
-})
-const passGuardado = 'contra'
-
-if (passGuardado == localStorage.getItem('pass')) {
-    createCards(data);
-
-}else {
-    document.body.append('Usuario o contraseña incorrectos, ingrese de nuevo.');
+// si el inicio de sesion esta bien
+    if (
+        emailGuardado === emailUsuario.value && passGuardado === passUsuario.value
+    ) {
+        createCards(data);
+    } else {
+        document.body.append(
+            "Usuario o contraseña incorrectos, ingrese de nuevo."
+        );
+    }
 }
+const btnLogin = document.getElementById("btnLogin");
+btnLogin.addEventListener("click", (event) => {
+    event.preventDefault();
+    showCards();
+});
+
 
 
